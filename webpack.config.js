8c84ffa0484b50ef66f7ba351d5c3d914b4b01a0
Johannes Ulén <johannes@rabaldermedia.se>
2017-04-06 15:39:27 +0200

    Made Webpack's public url changeable via PUBLIC_PATH.

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name]-[hash].js',
-        publicPath: '/'
+        publicPath: process.env.PUBLIC_PATH || '/'
     },
     module: {
         rules: [
