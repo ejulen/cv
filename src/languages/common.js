f884ff6f917f69aa0be9eb37031b8c8ae5cf91dd
Johannes Ulén <johannes@rabaldermedia.se>
2017-04-06 17:20:07 +0200

    Added some skills.

--- src/languages/common.js
+++ src/languages/common.js
@@ -13,7 +13,11 @@ module.exports = {
             'Webpack',
             'Ruby',
             'Python',
-            'Git'
+            'Git',
+            'Java',
+            'Angular',
+            'SQL',
+            'C#'
         ]
     },
     experience: {
